ed408b59a928b171e5be911a5bdd73cdb2e50fe2
a7med_mubarak <139309062+a7med-Mubarak@users.noreply.github.com>
2024-08-20 01:24:11 +0300

    refresh-issue-solve2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 import CategoriesList from './modules/Categories/components/CategoriesList/CategoriesList';
